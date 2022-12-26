9079bab5a55f0c10bf87e1fa8d01655f947efd17
suzi01 <suzannahthompson@hotmail.co.uk>
2022-12-26 16:00:12 +0000

    Working on basket Tests (part 3): Basket component functionality > Displays empty basket

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -6,7 +6,7 @@ import Product from './Product'
 const Basket = ({basket, ...props}) => {
   const {location, basketCount, basketTotal, removeFromBasket} = props
   // const { basketTotal} = total
-    // console.log('basket', basket)
+    console.log('basket', basket)
     // console.log('props',basketTotal)
     // console.log(total)
   return <>
@@ -19,7 +19,7 @@ const Basket = ({basket, ...props}) => {
     <BasketCount />    */}
     {/* {products.length !== 0 && products.map(product => <Product key={product.id} media={product} addToBasket={props.addToBasket}/>)} */}
     {/* {basket.length ===0 ? 'Sorry, no items in basket' : <Product location="basket" products={basket} />} */}
-    {basket === 0 ? 'Sorry, no items in basket..' : basket.map(basketItem =>
+    {basket === 0 || basket.length === 0 ? 'Sorry, no items in basket..' : basket.map(basketItem =>
       <Product location={location} key={basketItem.trackId} item={basketItem} addToBasket={null} removeFromBasket={removeFromBasket} inBasket={basketItem.inBasket} />
      )}
      <h1>Total: £{basketTotal === 0 ? `0.00`:<BasketTotal basketTotal={basketTotal} />}</h1>
