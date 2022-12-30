f8ed923eb2f1ef1c825ab37ab5495c74d2678fbc
suzi01 <suzannahthompson@hotmail.co.uk>
2022-12-30 17:27:46 +0000

    Installed history package for About.spec.js, undone changes to code

--- src/__tests__/About.spec.js
+++ src/__tests__/About.spec.js
@@ -5,18 +5,12 @@ import '@testing-library/jest-dom';
 import { createMemoryHistory } from 'history';
 import About from '../pages/About';
 
-// import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {useNavigate} from 'react-router-dom'
-
 describe("Components load as expected", () => {
   test('About page content', () => {
     const history = createMemoryHistory();
-    const navigate = useNavigate()
-    navigate('/about')
-    // history.push('/about');
-
+    history.push('/about');
     render(
-      <Router history={navigate}>
+      <Router history={history}>
         <About />
       </Router>,
     );
